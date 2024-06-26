7f25adace220bd5cb25c6334cb9426506100b114
Guilherme P. Pimentel <165931160+Guidjy@users.noreply.github.com>
2024-06-26 14:39:39 -0300

    Create heap.h

---
+++ fila_de_prioridade/heap.h
@@ -0,0 +1,19 @@
+#ifndef _HEAP_H_
+#define _HEAP_H_
+
+// número de nós máximos no heap
+#define TAM_HEAP 63
+
+typedef struct heap *heap_t;
+
+// cria uma fila de prioridade
+heap_t heap_cria();
+
+// insere um elemento na fila de prioridade
+void heap_insere(heap_t self, int dado);
+
+// remove o nó de maior prioridade (nó raiz)
+int heap_remove(heap_t self);
+
+
+#endif
